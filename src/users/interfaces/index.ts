90f65c91e5db62464d8378062bd9ccdcd2d728b2
Abiodun Michael <michaelmedewase@gmail.com>
2023-08-02 09:00:50 +0100

    add updated status

--- src/users/interfaces/index.ts
+++ src/users/interfaces/index.ts
@@ -7,7 +7,7 @@ export interface IUser{
     password:string
     createdAt: Date
     updatedAt: Date
-    isRevoked:boolean
+    isRevoked?:boolean
 }
 
 export type ICreateUserInput = Omit<IUser, 'id' | 'createdAt' | 'updatedAt'>
